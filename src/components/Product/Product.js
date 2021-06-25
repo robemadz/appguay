cffc10033b8b94b6ccac0f6a0b567af5254f831c
Front <front@iMac-3.local>
2021-06-25 21:29:21 +0200

    first upload

---
+++ src/components/Product/Product.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+
+export default class Product extends Component {
+    render() {
+        let {name,price} = this.props.details
+        return (
+            <section>
+                <h3>OFERTA</h3>
+                <p>Producto:{name}</p>
+                <p>Precio: {price} €</p>
+            </section>
+        )
+    }
+}
