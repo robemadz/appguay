dcdb1c337e45fb033450e856b2897ba3ac2e13f9
Front <front@iMac-3.local>
2021-06-28 20:35:48 +0200

    add functions

--- src/components/Products/Products.js
+++ src/components/Products/Products.js
@@ -1,21 +1,46 @@
 import React, { Component } from 'react'
 import Product from '../Product/Product'
+import data from '../../data.json'
 
 export default class Products extends Component {
 
-    render() {
+        constructor(props) {
+            super(props)
+        
+            this.state = {
+                name:"--",
+                 products:data //va a ser un array de json. Si no quiero una precarga, pondría products:[]
+            }
+        }
+        
 
-        let data = [{name:"botella", price:"10  "},
-        {name:"cosa", price:"3"},
-        {name:"Silla", price:"20", brand:"Ikea"},
-        {name:"queso", price:"4", brand:"Garcia Vaquero"}]
-return (
-<article>
-    <Product details={data[0]}/>
-    <Product details={data[1]}/>
-    <Product details={data[2]}/>
-    <Product details={data[3]}/>
-</article>
-)
-}
+        renderProducts = () =>
+            this.state.products.map((product, i)=> <Product details={product} key={i}/>)
+
+        addProduct = () => {
+            let name = prompt("Introduce nombre")
+            let price = prompt("Introduce precio")
+
+            let product = {name,price}
+
+            this.setState({name})
+            this.setState({products:[...this.state.products,product]})
+
+            alert(`Producto añadido: ${name} ${price}`)
+        }
+
+        removeAllProducts = () => this.setState({name:"--", products:[]})
+    
+
+        render() {
+            return (
+            <article>
+                {this.renderProducts()}
+                <button onClick={this.addProduct}>Añadir producto</button>
+                <button onClick={this.removeAllProducts}>Borrar todo</button>
+                <p>Último añadido: {this.state.name}</p>
+            </article>
+            
+        )
+    }
 }
